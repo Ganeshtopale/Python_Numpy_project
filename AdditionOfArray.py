80d0dad3e968a66aee73f977c6a864abf7401699
Ganeshtopale <111178888+Ganeshtopale@users.noreply.github.com>
2023-06-06 14:54:35 +0530

    Add files via upload
    Addition of Array

---
+++ AdditionOfArray.py
@@ -0,0 +1,7 @@
+import numpy as np
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([1, 2, 3])
+print("1st array : ", arr1)
+print("2nd array : ", arr2)
+addition = np.add(arr1, arr2)
+print("Addition Of Array : ", addition)
